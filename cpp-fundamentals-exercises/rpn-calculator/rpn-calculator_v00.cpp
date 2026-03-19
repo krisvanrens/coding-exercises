bf6bd5203bc21b12ec83a1ada10c766a85e62779
Kris van Rens <kris@vanrens.org>
2026-03-19 23:13:04 +0100

    CHG: Update move information.

--- cpp-fundamentals-exercises/rpn-calculator/rpn-calculator_v00.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-
-int main() {
-  std::cout << "TODO: Implement RPN calculator..\n";
-}
--- exploring-rust-exercises/raycasting/src/lib.rs
+++
@@ -1,8 +0,0 @@
-/// 2D position abstraction.
-pub mod position;
-
-/// Compile-time level map definition.
-pub mod map;
-
-/// Player state abstraction.
-pub mod player;
